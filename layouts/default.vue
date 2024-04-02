f2d3320d3a9dd431e3817c619a2a3bc80ec59499
Leandro Frederico <leandro.fred@territoriosaber.com>
2024-04-02 11:04:15 -0300

    chore: removed navbar and changed bg-color

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,8 +14,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="mx-auto max-w-2xl mb-12 px-4 pb-4 lg:px-8 bg-[#23282C] text-white">
-    <HomeNavbar />
+  <div class="mx-auto max-w-2xl mb-12 px-4 pb-4 lg:px-8 bg-[#23282D] text-white">
     <slot></slot>
   </div>
 </template>
